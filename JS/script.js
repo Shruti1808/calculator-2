f66a11720a172362bb25ca05f28b2e867f1f090c
Brandon Cooper and Shruti Priya <brandon@>
2017-01-11 13:26:46 -0800

    add div after form to display result

--- JS/script.js
+++ JS/script.js
@@ -1,34 +1,35 @@
-var add =function(number1,number2){
-return number1 + number2;
+var add =function(number1, number2){
+ return number1 + number2;
 };
-var subtract = function(number1,number2){
+var subtract = function(number1, number2){
   return number1 - number2;
 };
-var multiply = function(number1,number2){
+var multiply = function(number1, number2){
   return number1*number2;
 };
-var division = function(number1,number2){
+var division = function(number1, number2){
   return number1/number2;
 };
-var remainder=function(number1,number2){
+var remainder = function(number1, number2){
   return number1 % number2;
 };
-var power=function(number1,number2){
+var power = function(number1, number2){
   return Math.pow(number1,number2);
 };
 
-var result= var add + var subtract
-var number1= parseInt(prompt("Enter a number:"));
-var number2= parseInt(prompt("Enter another number:"));
+  $(document).ready(function() {
+    $("form#add").submit(function(event) {
+      event.preventDefault();
+      var number1 = parseInt($("#add1").val());
+      var number2 = parseInt($("#add2").val());
+      var result = add(number1, number2);
+      $("#output").text(result);
+    });
+  });
 
-alert("The sum of the two is" + add(number1, number2) + ".");
-alert("The subtraction of the two is" + subtract(number1, number2) + ".");
-alert("The multiply of the two is" + multiply(number1, number2) + ".");
-alert("The division of the two is" + division(number1, number2) + ".");
-alert("The remainder of the two is" + remainder(number1, number2) + ".");
-alert("The power of the two is" + power(number1, number2) + ".");
 
-
-// var result= "sum is: " + add(number1,number2)+" subtraction is   "+subtract(number1,number2)+" multiply is "+multiply(number1,number2)+" division is "+division(number1,number2)+" remainder is "+remainder(number1,number2)+" power is "+power(number1,number2);
-//
-// alert("the result of your calculation is "+result);
+  //alert("The subtraction of the two is " + add(number1, number2) + ".");
+  // alert("The multiply of the two is " + multiply(number1, number2) + ".");
+  // alert("The division of the two is " + division(number1, number2) + ".");
+  // alert("The remainder of the two is " + remainder(number1, number2) + ".");
+  // alert("The power of the two is " + power(number1, number2) + ".");
